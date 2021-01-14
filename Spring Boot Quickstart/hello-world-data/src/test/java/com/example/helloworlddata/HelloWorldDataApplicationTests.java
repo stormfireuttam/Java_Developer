bbffcd702d106ee2546c7a6c12f71ae84aaa3088
Uttam Mittal <uttam29mittal@gmail.com>
2021-01-14 17:47:01 +0530

    Add files via upload

---
+++ Spring Boot Quickstart/hello-world-data/src/test/java/com/example/helloworlddata/HelloWorldDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworlddata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
